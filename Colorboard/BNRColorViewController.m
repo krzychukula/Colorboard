ae474457c868a90d496799e53b3eebf5436383c1
krzychukula <krzychukula@gmail.com>
2014-11-15 11:12:21 +0100

    Add UI Elements
    Turns out this w:Any h:Any at the bottom of the storyboard is not for
    zooming :)

--- Colorboard/BNRColorViewController.m
+++ Colorboard/BNRColorViewController.m
@@ -8,11 +8,34 @@
 
 #import "BNRColorViewController.h"
 
+@interface BNRColorViewController ()
+
+@property (nonatomic, weak) IBOutlet UITextField *textField;
+
+@property (nonatomic, weak) IBOutlet UISlider *redSlider;
+@property (nonatomic, weak) IBOutlet UISlider *greenSlider;
+@property (nonatomic, weak) IBOutlet UISlider *blueSlider;
+
+@end
+
 @implementation BNRColorViewController
 
 - (IBAction)dismiss:(id)sender
 {
     [self.presentingViewController dismissViewControllerAnimated: YES
                                                       completion:nil];
+}
+
+- (IBAction)changeColor:(id)sender
+{
+    float red = self.redSlider.value;
+    float green = self.greenSlider.value;
+    float blue = self.blueSlider.value;
+    
+    UIColor *newColor = [UIColor colorWithRed:red
+                                        green:green
+                                         blue:blue
+                                        alpha:1.0];
+    self.view.backgroundColor = newColor;
 }
 @end
