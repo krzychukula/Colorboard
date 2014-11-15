6dadeaa790e995a790c330eb19949e86aa087135
krzychukula <krzychukula@gmail.com>
2014-11-15 09:17:32 +0100

    chapter 28 UIStoryboard
    Using Single view template for initial project structure

---
+++ Colorboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Colorboard
+//
+//  Created by Krzysztof Kula on 15/11/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
